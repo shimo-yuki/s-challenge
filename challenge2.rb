339cdb9889fed564df4cc620752b1756c33cf0cb
shimo-yu <yf521985@gmail.com>
2021-12-24 22:22:18 +0900

    fix: indent

--- challenge2.rb
+++ challenge2.rb
@@ -1,7 +1,7 @@
 # http://serviver-challenge.herokuapp.com/problems/25
 
 def main
-	puts '計算式を入力してください'
+  puts '計算式を入力してください'
   puts '終了する場合はexitと入力してください'
 
 loop do
@@ -16,54 +16,54 @@ loop do
 end
 
 def calc(calculation)
-	formula = []
-	kari = []
-	for i in 1..calculation.size do
-		case calculation[i - 1]
-		when /[0-9]/
-		case calculation[i]
-		when /[0-9]/
-			kari << calculation[i - 1]
-		else
-			if kari.empty?
-				formula << calculation[i - 1]
-			else
-				kari << calculation[i - 1]
-				formula << kari.join 
-			end
-		end
-		when /[+\-*\/]/
-			if /[+\-*\/]/ == calculation[i]
-				puts '正しい計算式ではありません'
-			else
-				formula << calculation[i -1]
-				kari = []
-			end
-		else
-			puts '正しい計算式ではありません'
-			break
-		end
-	end
-	sum(formula)
+  formula = []
+  kari = []
+  for i in 1..calculation.size do
+    case calculation[i - 1]
+    when /[0-9]/
+    case calculation[i]
+    when /[0-9]/
+      kari << calculation[i - 1]
+    else
+      if kari.empty?
+        formula << calculation[i - 1]
+      else
+        kari << calculation[i - 1]
+        formula << kari.join 
+      end
+    end
+    when /[+\-*\/]/
+      if /[+\-*\/]/ == calculation[i]
+        puts '正しい計算式ではありません'
+      else
+        formula << calculation[i -1]
+        kari = []
+      end
+    else
+      puts '正しい計算式ではありません'
+      break
+    end
+  end
+  sum(formula)
 end
 
 def sum(formula)
-	sumtotal = formula[0].to_i
-	formula.each_with_index do |f, i|
-		if i % 2 != 0
-			case f
-			when /[+]/
-				sumtotal = "#{sumtotal.to_i + formula[i + 1].to_i}"
-			when /[\-]/
-				sumtotal = "#{sumtotal.to_i - formula[i + 1].to_i}"
-			when /[*]/
-				sumtotal = "#{sumtotal.to_i * formula[i + 1].to_i}"
-			when /[\/]/
-				sumtotal = "#{sumtotal.to_i / formula[i + 1].to_i}"
-			end
-		end
-	end
-	puts sumtotal
+  sumtotal = formula[0].to_i
+  formula.each_with_index do |f, i|
+    if i % 2 != 0
+      case f
+      when /[+]/
+        sumtotal = "#{sumtotal.to_i + formula[i + 1].to_i}"
+      when /[\-]/
+        sumtotal = "#{sumtotal.to_i - formula[i + 1].to_i}"
+      when /[*]/
+        sumtotal = "#{sumtotal.to_i * formula[i + 1].to_i}"
+      when /[\/]/
+        sumtotal = "#{sumtotal.to_i / formula[i + 1].to_i}"
+      end
+    end
+  end
+  puts sumtotal
 end
 
 main
